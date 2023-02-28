a7b3ca2e51ba20f43807abe6f4622c95c1f6407c
jopimi <josepitarch98@gmail.com>
2023-02-28 23:56:40 +0100

    Change hamburguer icon

--- icons/MenuIcon.jsx
+++ icons/MenuIcon.jsx
@@ -1,25 +1,26 @@
 export default function MenuIcon() {
-  <>
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      class='w-8 h-8 first'
-      fill='none'
-      viewBox='0 0 24 24'
-      stroke='currentColor'
-      stroke-width='2'
-    >
-      <path stroke-linecap='round' stroke-linejoin='round' d='M4 6h16M4 12h8m-8 6h16'></path>
-    </svg>
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-8 h-8 first"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth={2}
+      >
+        <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h8m-8 6h16" />
+      </svg>
 
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      class='hidden w-8 h-8 last'
-      fill='none'
-      viewBox='0 0 24 24'
-      stroke='currentColor'
-      stroke-width='2'
-    >
-      <path stroke-linecap='round' stroke-linejoin='round' d='M6 18L18 6M6 6l12 12'></path>
-    </svg>
-  </>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="hidden w-8 h-8 last"
+        fill="none" viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth={2}
+      >
+        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+      </svg>
+    </>
+  )
 }
