bde87258b7307daaa94f6e993d7086b37612f75a
jopimi <josepitarch98@gmail.com>
2023-02-28 23:55:45 +0100

    Add font

---
+++ pages/_document.jsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,700,900;1,400&display=swap" rel="stylesheet"/>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
