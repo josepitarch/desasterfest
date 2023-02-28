77cefd2525ef340c33a2899b60e01cb4f3c9496a
jopimi <josepitarch98@gmail.com>
2023-02-28 23:56:08 +0100

    Add social icons

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,35 +1,60 @@
 import Image from "next/image"
 import Link from "next/link"
 import Logo from "../public/logo.png"
+import MenuIcon from "../icons/MenuIcon"
+import { Icons } from "./Icons"
 
-const NAVIGATION_LINKS = [{
-	title: 'Inicio',
-	href: '#top'
-}, {
-	title: 'Aspanion',
-	href: '#aspanion',
-}, {
-	title: 'Line Up',
-	href: '#line-up',
-}, {
-	title: 'I Edición',
-	href: '#first-edition',
-}]
+const NAVIGATION_LINKS = [
+	{
+		title: 'Inicio',
+		href: '#top'
+	},
+	{
+		title: 'Line Up',
+		href: '#line-up',
+	},
+	{
+		title: 'Aspanion',
+		href: '#aspanion',
+	},
+	{
+		title: 'I Edición',
+		href: '#first-edition',
+	},
+	{
+		title: 'Cómo llegar',
+		href: '#localization',
+	}
+]
+
+const SOCIAL_NETWORKS_LINKS = [
+	{
+		title: 'instagram',
+		icon: Icons.instagram,
+		href: 'https://instagram.com/desasterfest_'
+	},
+	{
+		title: 'facebook',
+		icon: Icons.facebook,
+		href: 'https://twitter.com/midudev'
+	}
+]
 
 export default function Navigation() {
 	return (
-		<aside className='flex flex-col-reverse content-center xl:sticky xl:top-24 xl:flex-col'>
+		<aside className='flex flex-col-reverse content-center xl:sticky xl:top-24 xl:flex-col p-2'>
 			<Link href='/#top' className='mt-[-50px] xl:mt-0'>
-				<Image src={Logo} id='logo' className='animate-zoomIn' alt='Logo Desaster Fest'/>
+				<Image src={Logo} id='logo' className='animate-zoomIn' alt='Logo Desaster Fest' />
 			</Link>
 			<input type='checkbox' hidden id='menu' className='peer' />
 			<label
 				htmlFor='menu'
-				className='z-50 self-end xl:hidden peer-checked:[&>.first]:rotate-45 peer-checked:[&>.third]:rotate-[-45deg] peer-checked:[&>.second]:opacity-0'
+				className='bg-zinc-500/30 w-14 h-14 border-zinc-500 flex items-center
+				justify-center rounded-full border relative z-50 xl:hidden text-primary
+				peer-checked:[&>.first]:hidden peer-checked:[&>.last]:block self-end
+				cursor-pointerhover:scale-125 transition hover:border-white'
 			>
-				<div className='first'></div>
-				<div className='second'></div>
-				<div className='third'></div>
+				<MenuIcon />
 			</label>
 
 			<nav id='navbar' className='last absolute top-0 left-0 right-0 z-40 hidden min-h-screen py-24 px-8 bg-black/30 peer-checked:block backdrop-blur-lg text-center xl:px-0 xl:flex xl:flex-col xl:min-h-min xl:backdrop-blur-0 xl:mt-24 transition-all'>
@@ -42,6 +67,18 @@ export default function Navigation() {
 						})
 					}
 				</ul>
+
+				<div className='flex flex-row items-center justify-center gap-4 py-2'>
+					{
+						SOCIAL_NETWORKS_LINKS.map(({ icon: Icon, href, title }) => {
+							return <a key={title} className='text-white hover:scale-125 transition-all hover:text-primary' href={href} target='_blank' rel='noopener noreferrer'>
+								<span aria-label={title}>
+									<Icon />
+								</span>
+							</a>
+						})
+					}
+				</div>
 			</nav>
 		</aside>
 	)
