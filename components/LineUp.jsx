b03c9c488c0029dc225764a041911ab9f27c0a4d
jopimi <josepitarch98@gmail.com>
2023-03-18 18:07:37 +0100

    updte line up

--- components/LineUp.jsx
+++ components/LineUp.jsx
@@ -4,32 +4,47 @@ const artists = [
 	{
 		id: 1,
 		name: 'Monica X',
-		description: 'DJ de la discoteca de la ruta Chocolate',
 		image: '/line-up/monica.jpg'
 	},
 	{
 		id: 2,
 		name: 'Batiste',
-		description: 'DJ del pueblo',
 		image: '/line-up/batiste.jpg'
 	},
 	{
 		id: 3,
 		name: 'Paco Caña',
-		description: 'DJ de la discoteca Masia',
 		image: '/line-up/pacocana.jpg'
 	},
 	{
 		id: 4,
 		name: 'Serrano',
-		description: 'DJ de la discoteca Masia',
 		image: '/line-up/serrano.jpg'
 	},
 	{
 		id: 5,
 		name: 'Ismael Lora',
-		description: 'DJ de la discoteca Masia',
 		image: '/line-up/lora.jpg'
+	},
+	{
+		id: 6,
+		name: 'Abel K Kaña',
+		image: '/line-up/abelkkana.jpg'
+	},
+	{
+		id: 7,
+		name: 'Paco Banaclocha',
+		image: '/line-up/banaclocha.jpg'
+	},
+	{
+		id: 8,
+		name: 'Juanka',
+		image: '/line-up/juanka.jpg'
+	},
+	{
+		id: 9,
+		name: 'Peku',
+		image: '/line-up/peku.jpg'
 	},
 ]
 
@@ -41,12 +56,12 @@ export default function LineUp() {
 				{
 					artists.map(artist => (
 						<li key={artist.id} className='bg-white rounded-lg overflow-hidden'>
-								<img
-									src={artist.image}
-									className='img-line-up'
-									alt={artist.name}
-									/>
-								<h3 className='font-bold m-3'>{artist.name}</h3>
+							<img
+								src={artist.image}
+								className='img-line-up'
+								alt={artist.name}
+							/>
+							<h3 className='font-bold m-3'>{artist.name}</h3>
 						</li>
 					))
 				}
--- components/NewConfirmation.jsx
+++ components/NewConfirmation.jsx
@@ -5,13 +5,13 @@ export default function NewConfirmation() {
       <h2 className='text-3xl'>Nueva confirmación</h2>
       <div className='bg-white overflow-hidden rounded-lg mt-2'>
         <img
-          src='/line-up/lora.jpg'
+          src='/line-up/abelkkana.jpg'
           height='600'
           width='400'
         />
         <div className='p-3'>
-          <h4 className='font-bold'>Ismael Lora</h4>
-          <p className='italic text-gray-500'>5º artista confirmado</p>
+          <h4 className='font-bold'>Abel K Kaña</h4>
+          <p className='italic text-gray-500'>9º artista confirmado</p>
         </div>
       </div>
     </section>
