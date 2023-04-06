9cc44f0143d97bde02d5969e2a02ce15efbfeaea
jopimi <josepitarch98@gmail.com>
2023-04-07 00:57:17 +0200

    update new confirmation

--- components/NewConfirmation.jsx
+++ components/NewConfirmation.jsx
@@ -5,12 +5,12 @@ export default function NewConfirmation() {
       <h2 className='text-3xl'>Nueva confirmación</h2>
       <div className='bg-white overflow-hidden rounded-lg mt-2'>
         <img
-          src='/line-up/tonin.jpg'
+          src='/line-up/raul-ortiz.jpg'
           height='600'
           width='400'
         />
         <div className='p-3'>
-          <h4 className='font-bold'>Toñin by Jumper Brothers</h4>
+          <h4 className='font-bold'>Raúl Ortiz</h4>
           <p className='italic text-gray-500'>14º artista confirmado</p>
         </div>
       </div>
