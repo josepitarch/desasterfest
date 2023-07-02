8d4beb5fa04cd69d536b15722602d9b893fd98c8
jopimi <josepitarch98@gmail.com>
2023-07-02 15:07:58 +0200

    add master class content

--- components/NewConfirmation.jsx
+++ components/NewConfirmation.jsx
@@ -1,15 +1,31 @@
 
+const URL_MEGAENTRADAS = 'https://megaentradas.com/evento/desasterfest-almenara'
+
 export default function NewConfirmation() {
   return (
     <section id='new-confirmation' className='mt-20 flex flex-col items-center p-4'>
-      <h2 className='text-3xl text-center mb-2'>¡Ya disponible el video presentación!</h2>
-      <iframe width={'80%'}
+      <h2 className='text-3xl text-center mb-2'>Master class ciclo indoor</h2>
+      {/* <iframe width={'80%'}
         className='h-[400px] lg:h-[500px]'
         src="https://www.youtube.com/embed/WomH3uMIbPU"
         title="Video presentación Desaster Fest"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen>
-      </iframe>
+      </iframe> */}
+      <p>
+        Ahora por la mañana master class de ciclo indoor. Tres horas disfrutando de la música de Monica X con nuestras reputadas
+        monitoras <strong>Vera Almero</strong>, <strong>Tania Melendo</strong> y <strong>Noelia Fenolloda</strong>. Habrán
+        descansos de 15 minutos entre cada sesión donde ofreceremos refrescos. ¡Apúntate ya! Y no olvides adquirir tu entrada
+        para el evento de la tarde.
+      </p>
+      <a
+        href={URL_MEGAENTRADAS}
+        target="_blank"
+        className='bg-primary p-4 mt-10 w-72 lg:w-96 rounded-xl text-white text-center text-2xl font-bold'
+      >
+        Comprar entradas ciclo indoor
+      </a>
+      <img className='mt-2' src='/indoor.jpg' alt='Poster master class ciclo indoor' />
     </section>
   )
 }
